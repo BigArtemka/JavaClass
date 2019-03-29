f494ee719cc0efa1421440a38ec749365c47ac81
BigArtemka <dak.12008@gmail.com>
2019-03-30 01:08:12 +0300

    class

--- src/main/java/decimalNumber/Number.java
+++ src/main/java/decimalNumber/Number.java
@@ -1,11 +1,14 @@
 package decimalNumber;
 
-import com.sun.istack.internal.NotNull;
+
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import static java.lang.Integer.max;
 import static java.lang.Integer.parseInt;
 
 public class Number {
@@ -13,7 +16,7 @@ public class Number {
     private int scale;
     final private int base = 1000000000;
 
-    public Number(List<Integer> num, int scale) {//list
+    public Number(List<Integer> num, int scale) {
         this.num = num;
         this.scale = scale;
     }
@@ -68,11 +71,59 @@ public class Number {
     }
 
     public Number add(@NotNull Number other) {
-        return new Number("13.13");
+        List<Integer> dec1 = this.getBefPoint();
+        List<Integer> dec2 = other.getBefPoint();
+        List<Integer> cel1 = this.getAftPoint();
+        List<Integer> cel2 = other.getAftPoint();
+        return new Number(listAdd(cel1, cel2), scale);
+
     }
 
-    // NotNull
-    private static List<Integer> toListInt(List<Long> a) {
+    private List<Integer> getBefPoint() {
+        List<Integer> res = new ArrayList<Integer>();
+        int counter = 0;
+        for (int i = 0; i < num.size(); i += 1) {
+            counter += 9;
+            if (counter > scale) {
+                res.add((int) (num.get(i) % Math.pow(10, scale % 9)));
+                break;
+            }
+            else res.add(num.get(i));
+        }
+        return res;
+    }
+
+    private List<Integer> getAftPoint() {
+        List<Integer> res = new ArrayList<Integer>();
+        int counter = num.size() * 9;
+        for (int i = num.size() - 1; i >= 0; i--) {
+            counter -= 9;
+            if (counter < scale) {
+                res.add((int) (this.num.get(i) / Math.pow(10, scale - counter)));
+                break;
+            }
+            else res.add(num.get(i));
+        }
+        return res;
+    }
+
+    private static List<Integer> listAdd(@NotNull List<Integer> th, @NotNull List<Integer> other) {
+        int counter = 0;
+
+        int base = 1000000000;
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        for (int i = 0; i < max(th.size(), other.size()) || counter == 1; i++) {
+            res.add(0);
+            res.set(i,  th.get(i) + counter + (i < other.size() ? other.get(i) : 0));
+            if (res.get(i) >= base)
+                counter = 1;
+            else counter = 0;
+            if (counter == 1) res.set(i, res.get(i) - base);
+        }
+        return res;
+    }
+
+    private static List<Integer> toListInt(@NotNull List<Long> a) {
         List<Integer> res = new ArrayList<Integer>();
         StringBuilder str = new StringBuilder();
         for (int i = a.size() - 1; i >= 0; i--) {
@@ -91,12 +142,12 @@ public class Number {
     }
 
     private static int getCountsOfDigits(long number) {
-        return (number == 0) ? 1 : (int) Math.ceil(Math.log10(number + 0.5)); //pack names
+        return (number == 0) ? 1 : (int) Math.ceil(Math.log10(number + 0.5));
     }
 
     public static void main(String[] args) {
-        Number n = new Number("0.04367346340000000006437685856865834");
-        Number n1 = new Number("0.04367346340000000006437685856865834");
-        System.out.println(n.mul(n1).toString());
+        Number n = new Number("4367365465416516216464.0");
+        Number n1 = new Number("4564565465.0");
+        System.out.println(n.add(n1).toString());
     }
 }
