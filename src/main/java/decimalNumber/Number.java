f2cf055c372159c41645e7d1e9de787a7de77b63
BigArtemka <dak.12008@gmail.com>
2019-04-11 00:46:19 +0300

    ne class

--- src/main/java/decimalNumber/Number.java
+++ src/main/java/decimalNumber/Number.java
@@ -5,9 +5,11 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 import static java.lang.Integer.max;
+import static java.lang.Integer.min;
 import static java.lang.Integer.parseInt;
 import static java.lang.Math.pow;
 
@@ -27,16 +29,54 @@ public class Number {
             if (Pattern.compile("^\\s*-?[0-9]+\\s*$").matcher(n[0]).matches()
                     && Pattern.compile("^\\s*[0-9]+\\s*$").matcher(n[1]).matches()) {
                 scale = n[1].length();
-                String s = n[0] + n[1];
-                for (int i = s.length(); i > 0; i -= 9) {
-                    if (i - 9 < 0)
-                        this.num.add(parseInt(s.substring(0, i)));
-                    else this.num.add(parseInt(s.substring(i - 9, i)));
+                StringBuilder str = new StringBuilder(n[0] + n[1]);
+                while (scale > 0 && str.charAt(str.length() - 1) == '0') {
+                    str.deleteCharAt(str.length() - 1);
+                    scale--;
                 }
-            }
+                boolean minus = str.charAt(0) == '-';
+                if (minus) str.deleteCharAt(0);
+                for (int i = str.length(); i > 0; i -= 9) {
+                    if (i - 9 < 0)
+                        this.num.add(parseInt(str.substring(0, i)));
+                    else this.num.add(parseInt(str.substring(i - 9, i)));
+                }
+                if (minus) this.num.set(this.num.size() - 1, this.num.get(this.num.size() - 1) * -1);
+            } else throw new NumberFormatException("Not number");
 
         } else throw new NumberFormatException("Not number");
     }
+
+    Number(@NotNull Integer num) {
+        int n = num;
+        if (n == 0) this.num.add(0);
+        while (n != 0) {
+            this.num.add(n % base);
+            n /= base;
+        }
+    }
+
+    Number(@NotNull Long num) {
+        long n = num;
+        if (n == 0) this.num.add(0);
+        while (n != 0) {
+            this.num.add((int) (n % base));
+            n /= base;
+        }
+    }
+
+    Number(@NotNull Float num) {
+        Number n = new Number(num.toString());
+        this.num = n.num;
+        this.scale = n.scale;
+    }
+
+    Number(@NotNull Double num) {
+        Number n = new Number(num.toString());
+        this.num = n.num;
+        this.scale = n.scale;
+    }
+
 
     @Override
     public String toString() {
@@ -55,6 +95,37 @@ public class Number {
         while (str.charAt(0) == '0' && str.charAt(1) != '.') str.deleteCharAt(0);
         return str.toString();
     }
+
+    public float toFloat() {
+        return Float.parseFloat(this.toString());
+    }
+
+    public double toDouble() {
+        return Double.parseDouble(this.toString());
+    }
+
+    public int toInt() {
+        int res = 0;
+        for (int i = 0; i <= scale - 9; i += 9)
+            num.remove(0);
+        num.set(num.size() - 1, num.get(num.size() - 1 / (int) pow(10, scale % 9)));
+        for (int i = 0; i < num.size(); i++)
+            res += num.get(i) * (int) pow(base, i);
+
+        return res;
+    }
+
+    public long toLong() {
+        long res = 0L;
+        for (int i = 0; i <= scale - 9; i += 9)
+            num.remove(0);
+        num.set(num.size() - 1, num.get(num.size() - 1 / (int) pow(10, scale % 9)));
+        for (int i = 0; i < num.size(); i++)
+            res += num.get(i) * (long) pow(base, i);
+
+        return res;
+    }
+
 
     public Number mul(@NotNull Number other) {
         long counter = 0;
@@ -75,8 +146,18 @@ public class Number {
             other.num.set(other.num.size() - 1, other.num.get(other.num.size() - 1) * -1);
             return this.sub(other);
         }
-        List<Integer> dec1 = this.getAftPoint();
-        List<Integer> dec2 = other.getAftPoint();
+        List<Integer> dec1;
+        List<Integer> dec2;
+        int maxScale;
+        if (this.scale > other.scale) {
+            dec1 = this.getAftPoint(0);
+            dec2 = other.getAftPoint((this.scale - other.scale) % 9);
+            maxScale = this.scale;
+        } else {
+            dec1 = this.getAftPoint((other.scale - this.scale) % 9);
+            dec2 = other.getAftPoint(0);
+            maxScale = other.scale;
+        }
         List<Integer> cel1 = this.getBefPoint();
         List<Integer> cel2 = other.getBefPoint();
         while (dec1.size() != dec2.size() || dec1.size() < 1) {
@@ -86,12 +167,44 @@ public class Number {
         }
         List<Integer> resDec = listAdd(dec1, dec2);
         List<Integer> resCel = listAdd(cel1, cel2);
+
+        //Если результат сложения дробных частей >= 1.0, перебрасываем целую часть в resCel
+        int resDecLast = resDec.get(resDec.size() - 1);     //последний элемент resDec
+        int countOfDigitDecLast = getCountsOfDigits(dec1.get(dec1.size() - 1)); //количество цифр в последнем элементе dec1
         if (resDec.size() > dec1.size()) {
             resCel = listAdd(resCel, resDec.subList(resDec.size() - 1, resDec.size()));
             resDec.remove(resDec.size() - 1);
+        } else if (getCountsOfDigits(resDecLast) > countOfDigitDecLast) {
+            List<Integer> o = new ArrayList<>();
+            o.add(1);
+            resDec.set(resDec.size() - 1, resDecLast % (int) pow(10, countOfDigitDecLast));
+            resCel = listAdd(resCel, o);
         }
-        resDec.addAll(resCel);
-        return new Number(resDec, max(scale, other.scale) + 9 - max(scale, other.scale) % 9);
+
+        //Избавляемся от незначащих нулей в дробной части
+        while (!resDec.isEmpty() && resDec.get(0) == 0)
+            if (resDec.size() == 1) {
+                resDec.remove(0);
+                maxScale = 0;
+            } else {
+                resDec.remove(0);
+                maxScale -= 9;
+            }
+        while (resDec.get(0) % 10 == 0) {
+            resDec.set(0, resDec.get(0) / 10);
+            maxScale--;
+        }
+
+        //соединяем resDec и resCel в один ArrayList
+        if (resDec.isEmpty()) resDec.addAll(resCel);
+        else
+            for (int i = 0; i < resCel.size(); i++) {
+                resDec.set((resDec.size() - 1), resDec.get(resDec.size() - 1) + resCel.get(i)
+                        % (int) pow(10, 9 - maxScale % 9) * (int) pow(10, maxScale % 9));
+                int newElem = resCel.get(i) / (int) pow(10, 9 - maxScale % 9);
+                if (newElem != 0) resDec.add(newElem);
+            }
+        return new Number(resDec, maxScale);
     }
 
 
@@ -100,8 +213,18 @@ public class Number {
             other.num.set(other.num.size() - 1, other.num.get(other.num.size() - 1) * -1);
             return this.add(other);
         }
-        List<Integer> dec1 = this.getAftPoint();
-        List<Integer> dec2 = other.getAftPoint();
+        List<Integer> dec1;
+        List<Integer> dec2;
+        int maxScale;
+        if (this.scale > other.scale) {
+            dec1 = this.getAftPoint(0);
+            dec2 = other.getAftPoint((this.scale - other.scale) % 9);
+            maxScale = this.scale;
+        } else {
+            dec1 = this.getAftPoint((other.scale - this.scale) % 9);
+            dec2 = other.getAftPoint(0);
+            maxScale = other.scale;
+        }
         List<Integer> cel1 = this.getBefPoint();
         List<Integer> cel2 = other.getBefPoint();
         while (dec1.size() != dec2.size() || dec1.size() < 1) {
@@ -109,25 +232,74 @@ public class Number {
                 dec1.add(0, 0);
             else dec2.add(0, 0);
         }
+
         List<Integer> resDec;
         List<Integer> resCel;
-        if (cel2.size() > cel1.size() || cel2.size() == cel1.size() && (cel2.get(cel2.size() - 1) > cel1.get(cel1.size() - 1)
-                || (cel2.get(cel2.size() - 1) == cel1.get(cel1.size() - 1) && dec2.get(dec2.size() - 1) > dec1.get(dec1.size() - 1)))) {
+        int cel1Last = 0;
+        int cel2Last = 0;
+        if (cel1.size() > 0) cel1Last = cel1.get(cel1.size() - 1);
+        if (cel2.size() > 0) cel2Last = cel2.get(cel2.size() - 1);
+        boolean celSizesEquals = cel2.size() == cel1.size();
+
+        /*
+        Если второе число больше первого, то мы вычитаем из него первое число,
+        при этом если деситичная часть первого числа больше, чем второго, мы
+        вычитаем из resCel единицу. Результат получится отрицательным, поэтому
+        умножаем старший элемент результата на -1.
+        Если наоборот, то вычитаем из первого второе, при этом если десятичная
+        часть второго числа больше, чем первого, мы вычитаем из resCel единицу.
+        Также избавляемся от незначимых нулей целой части числа.
+         */
+        if (cel2.size() > cel1.size() ||
+                celSizesEquals && cel2Last > cel1Last ||
+                celSizesEquals && cel2Last == cel1Last
+                        && dec2.get(dec2.size() - 1) > dec1.get(dec1.size() - 1)) {
             resCel = listSub(cel2, cel1);
             resDec = listSub(dec2, dec1);
-            if (dec1.get(dec1.size() - 1) > dec2.get(dec2.size() - 1))
-                resCel.set(0, resCel.get(0) - 1);
-            resCel.set(resCel.size() - 1, resCel.get(resCel.size() - 1) * -1);
-        }
-        else {
+            if (dec1.get(dec1.size() - 1) > dec2.get(dec2.size() - 1)) {
+                List<Integer> s = new ArrayList<>();
+                s.add(1);
+                resCel = listSub(resCel, s);
+            }
+            while (!resCel.isEmpty() && resCel.get(0) == 0) resCel.remove(0);
+            if (resCel.isEmpty()) resDec.set(resDec.size() - 1, resDec.get((resDec.size() - 1)) * -1);
+            else resCel.set(resCel.size() - 1, resCel.get(resCel.size() - 1) * -1);
+        } else {
             resCel = listSub(cel1, cel2);
             resDec = listSub(dec1, dec2);
-            if (dec1.get(dec1.size() - 1) < dec2.get(dec2.size() - 1))
-                resCel.set(0, resCel.get(0) - 1);
+            if (dec1.get(dec1.size() - 1) < dec2.get(dec2.size() - 1)) {
+                List<Integer> s = new ArrayList<>();
+                s.add(1);
+                resCel = listSub(resCel, s);
+        }
+            while (!resCel.isEmpty() && resCel.get(0) == 0) resCel.remove(0);
         }
 
-        resDec.addAll(resCel);
-        return new Number(resDec, max(scale, other.scale) + 9 - max(scale, other.scale) % 9);
+        //Избавляемся от незначащих нулей в дробной части
+        while (!resDec.isEmpty() && resDec.get(0) == 0)
+            if (resDec.size() == 1) {
+                resDec.remove(0);
+                maxScale = 0;
+            } else {
+                resDec.remove(0);
+                maxScale -= 9;
+            }
+        while (resDec.get(0) % 10 == 0) {
+            resDec.set(0, resDec.get(0) / 10);
+            maxScale--;
+        }
+
+        //соединяем resDec и resCel в один ArrayList
+        if (resDec.isEmpty()) resDec.addAll(resCel);
+        else
+            for (int i = 0; i < resCel.size(); i++) {
+                resDec.set((resDec.size() - 1),
+                        (resCel.get(i) < 0 ? -resDec.get(resDec.size() - 1) : resDec.get(resDec.size() - 1))
+                                + resCel.get(i) % (int) pow(10, 9 - maxScale % 9) * (int) pow(10, maxScale % 9));
+                int newElem = resCel.get(i) / (int) pow(10, 9 - maxScale % 9);
+                if (newElem != 0) resDec.add(newElem);
+            }
+        return new Number(resDec, maxScale);
     }
 
     private List<Integer> getBefPoint() {
@@ -144,16 +316,26 @@ public class Number {
         return res;
     }
 
-    private List<Integer> getAftPoint() {
+    private List<Integer> getAftPoint(int addedZeros) {
         List<Integer> res = new ArrayList<>();
         int counter = 0;
+        int a = (int) pow(10, 9 - addedZeros);
+        int b = (int) pow(10, addedZeros);
         for (int i = 0; i < num.size(); i++) {
             counter += 9;
-            res.add(num.get(i) % (int) pow(10, scale % 9) * (int) pow(10, 9 - scale % 9)
-                    + (i > 0 ? num.get(i - 1) / (int) pow(10, scale % 9) : 0));
-            if (counter > scale) break;
+            if (counter > scale) {
+                //отделяем от элемента дробную часть и заносим ее в массив
+                int decPart = num.get(i) % (int) pow(10, scale % 9);
+                res.add(decPart % a * b +
+                        (i > 0 ? num.get(i - 1) / a : 0));
+                if (decPart / a != 0)
+                    res.add(decPart / a);
+                break;
+            } else {
+                res.add(num.get(i) % a * b +
+                        (i > 0 ? num.get(i - 1) / a : 0));
+            }
         }
-        if (res.get(res.size() - 1) == 0) res.remove(res.size() - 1);
         return res;
     }
 
@@ -182,7 +364,11 @@ public class Number {
             if (res.get(i) < 0)
                 counter = 1;
             else counter = 0;
-            if (counter == 1) res.set(i, res.get(i) + base);
+            if (counter == 1)
+                if (i == max(th.size(), other.size()) - 1)
+                    res.set(i, res.get(i) + (int) pow(10, getCountsOfDigits(res.get(i))));
+                else
+                    res.set(i, res.get(i) + base);
         }
         return res;
     }
@@ -207,13 +393,30 @@ public class Number {
     }
 
     private static int getCountsOfDigits(long number) {
-        return (number == 0) ? 1 : (int) Math.ceil(Math.log10(number + 0.5));
+        int count = 0;
+        long n = number;
+        while (n != 0) {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Number number = (Number) o;
+        return scale == number.scale &&
+                Objects.equals(num, number.num);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(num, scale);
     }
 
     public static void main(String[] args) {
-        Number n = new Number("1000.6");
-        Number n1 = new Number("10000000000000000000000.5");
-        System.out.println(n.sub(n1).toString());
     }
 
 
--- src/test/java/decimalNumber/NumberTest.java
+++ src/test/java/decimalNumber/NumberTest.java
@@ -6,16 +6,34 @@ import static org.junit.Assert.*;
 
 public class NumberTest {
 
-    @org.junit.Test
+    @Test
     public void mul() {
-
+        Number n1 = new Number("9.999999999999");
+        Number n2 = new Number(1.0);
+        Number res = new Number("9.999999999999");
+        assertEquals(res, n1.mul(n2));
     }
 
-    @org.junit.Test
+    @Test
     public void add() {
-        Number n1 = new Number("213.004");
-        Number n2 = new Number("0.12342142");
-        assertEquals( n1.add(n2),new Number("213.12742142"));
+        Number n1 = new Number("5.59999999999999");
+        Number n2 = new Number("5.00000000000001");
+        Number res = new Number("10.6");
+        assertEquals(res, n1.add(n2));
+    }
 
+    @Test
+    public void sub() {
+        Number n1 = new Number("2.4");
+        Number n2 = new Number("3.6");
+        assertEquals(new Number(-1.2), n1.sub(n2));
+        assertEquals(new Number(1.2), n2.sub(n1));
+        Number n3 = new Number("1000.4");
+        Number n4 = new Number("988.65");
+        assertEquals(new Number("11.75"), n3.sub(n4));
+        assertEquals(new Number("-11.75"), n4.sub(n3));
+        Number n5 = new Number("1.0" +
+                "+");
+        Number n6 = new Number("0.000000000000000000000000001");
     }
 }
